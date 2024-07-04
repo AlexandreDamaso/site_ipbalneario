9cd96d5ae635fd80724676ec29d008f110775041
Alexandre Damaso <alexandredamasocosta@gmail.com>
2024-07-04 18:19:30 -0300

    Iniciado o footer

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+let count = 1;
+let qtdImage = 3;
+
+document.getElementById('radio-1').checked = true;
+
+setInterval(function() {
+   nextImage();
+}, 5000)
+
+function nextImage() {
+   count++;
+   if(count > qtdImage) {
+      count = 1;
+   }
+   document.getElementById('radio-' + count).checked = true;
+}
