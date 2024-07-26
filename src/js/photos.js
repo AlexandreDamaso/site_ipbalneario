f3ded291066be87beac772b629aea20d32f37269
Alexandre Damaso <alexandredamasocosta@gmail.com>
2024-07-25 22:59:31 -0300

    fotos

---
+++ src/js/photos.js
@@ -0,0 +1,18 @@
+// let photosCount = 1;
+// let photosQtdImage = 3;
+
+// document.getElementById('photos-radio-1').checked = true;
+
+// setInterval(function() {
+//    photosNextImage();
+// }, 1000)
+
+// function photosNextImage() {
+//    photosCount++;
+
+//    if(photosCount > photosQtdImage) {
+//       photosCount = 1;
+//    }
+   
+//    document.getElementById('photos-radio-' + photosCount).checked = true;
+// }
